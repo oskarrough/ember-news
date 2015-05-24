a45c8bd93b658f9ff4307c920a2a04fe622235d8
Oskar Rough <oskarrough@gmail.com>
2015-05-25 00:45:32 +0200

    Fix sorting

--- app/recent/route.js
+++ app/recent/route.js
@@ -3,7 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model() {
 		return this.store.find('post').then((posts) => {
-			return posts.sortBy('created').reverseObjects();
+
+			// this doesn't update when models are added or removed
+			// return posts.sortBy('created').reverseObjects();
+
+
+			return Ember.ArrayProxy.extend({
+				arrangedContent: Ember.computed.sort('content', 'sortProperties'),
+				sortProperties: ['created:desc']
+			}).create({
+				content: posts
+			});
 		});
 	}
 });
--- app/top/route.js
+++ app/top/route.js
@@ -3,7 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model() {
 		return this.store.find('post').then((posts) => {
-			return posts.sortBy('points').reverseObjects();
+			return Ember.ArrayProxy.extend({
+				arrangedContent: Ember.computed.sort('content', 'sortProperties'),
+				sortProperties: ['points:desc']
+			}).create({
+				content: posts
+			});
 		});
+
 	}
 });
