0fe488ffd373a1a271d06fa6a3565f0b9b1d56ff
Oskar Rough <oskarrough@gmail.com>
2015-05-25 00:24:54 +0200

    Add sorting

--- app/top/route.js
+++ app/top/route.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		return this.store.find('post');
+		return this.store.find('post').then((posts) => {
+			return posts.sortBy('points').reverseObjects();
+		});
 	}
 });
